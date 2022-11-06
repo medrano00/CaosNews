76b7bdcebcd2fd9b60c42dc6a870011841315253
Fabián Espinoza M <faboespinozam@icloud.com>
2022-11-06 20:02:08 -0300

    :3

---
+++ php/sql-xss.php
@@ -0,0 +1,14 @@
+<?php
+// Se definen las variables a utilizar
+$email = $password = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $email = test_input($_POST["email"]);
+  $password = test_input($_POST["password"]);
+}
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
